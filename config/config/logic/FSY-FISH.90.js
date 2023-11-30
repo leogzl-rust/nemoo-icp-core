5dada3fda225a92579a8ac7eb05ad3b4810fbc95
LG <git@LG>
2023-11-30 18:47:50 +0700

    From bjo-sea01 add fish rest 2023-11-30.184747

--- config/config/logic/FSY-FISH.90.js
+++ config/config/logic/FSY-FISH.90.js
@@ -1,14 +1,12 @@
-let url = "https://nemoo.bajo.id/v1/saveFish";
+url = "https://nemoo.bajo.id/v1/saveFish";
 
-let fish = {
+fish = {
     "fish": {
         "id": _.UNIQUE_ID,
         "fisher": "-",
         "weight": 100,
-        "height": 90
-    }
-}
+        "height": 90,
+    },
+};
 
-let result = restClient.execute(url, fish, String.class);
-
-System.out.println("RESULT: " + result);
+result = restClient.execute(url, fish);
